ab4bd215a8c9923ecc7cb8ae462edac2cca6c586
Adelaide <bsharah.a@northeastern.edu>
2023-02-15 19:44:17 -0500

    attempt 1

---
+++ ic-07.js
@@ -0,0 +1,45 @@
+// Let's create a visualization with a point for each datum 
+// in the following dataset 
+const data1 = [55000, 48000, 27000, 66000, 90000];
+
+const VIS_HEIGHT = FRAME_HEIGHT - MARGINS.top - MARGINS.bottom;
+const VIS_WIDTH = FRAME_WIDTH - MARGINS.left - MARGINS.right; 
+
+const FRAME1 = d3.select("#vis1")
+                  .append("svg")
+                    .attr("height", FRAME_HEIGHT)
+                    .attr("width", FRAME_WIDTH)
+                    .attr("class", "frame"); 
+
+// Now, let's define our scaling function
+
+// find max X
+const MAX_X = d3.max(data1, (d) => { return d; }); 
+console.log("Max x: " +MAX_X);  
+
+// Now, define scale functions that maps our data values 
+// (domain) to pixel values (range)
+const X_SCALE = d3.scaleLinear() // linear scale because we have 
+                              // linear data 
+                  .domain([0, (MAX_X + 10000)]) // add some padding  
+                  .range([0, VIS_WIDTH]); 
+
+console.log("Input: 40000, X_SCALE output: " + X_SCALE(40000));
+
+// Now, we can use X_SCALE to plot our points
+FRAME1.selectAll("points")  
+    .data(data2)  
+    .enter()       
+    .append("circle")  
+      .attr("cx", (d) => { return (X_SCALE(d) + MARGINS.left); }) 
+      .attr("cy", MARGINS.top) 
+      .attr("r", 20)
+      .attr("class", "point"); 
+
+// We can also use X_SCALE to add an axis to the vis  
+FRAME1.append("g") // g is a "placeholder" svg
+      .attr("transform", "translate(" + MARGINS.left + 
+            "," + (VIS_HEIGHT + MARGINS.top) + ")") //moves axis 
+                                                    // within margins 
+      .call(d3.axisBottom(X_SCALE).ticks(4)) // function for generating axis  
+        .attr("font-size", '20px'); // set font size
